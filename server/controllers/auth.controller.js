2bdaeb55cd4ba12b079273d6c01279f5bebb3930
Raj Ratn Gupta <raj.professional.id.engineer@gmail.com>
2026-07-22 19:09:55 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,8 +16,8 @@ export const googleAuth=async (req,res) => {
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"lax",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000
         })
 
@@ -32,8 +32,8 @@ export const logOut=async (req,res) =>{
     try {
         res.clearCookie('token',{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict"
+            secure:true,
+            sameSite:"none"
         });
 
         return res.status(200).json({message :"logout successfully"})
