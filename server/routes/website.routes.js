7a5c18894303841f2a6925cf19ba598e9d587da9
Raj Ratn Gupta <raj.professional.id.engineer@gmail.com>
2026-07-22 20:02:09 +0530

    Update website.routes.js

--- server/routes/website.routes.js
+++ server/routes/website.routes.js
@@ -10,5 +10,5 @@ websiteRouter.post("/update/:id",isAuth,changes)
 websiteRouter.get("/get-by-id/:id",isAuth,getWebsiteById)
 websiteRouter.get("/get-all",isAuth,getAll)
 websiteRouter.get("/deploy/:id",isAuth,deploy)
-websiteRouter.get("/get-by-slug/:slug",isAuth,getBySlug)
+websiteRouter.get("/get-by-slug/:slug",getBySlug)
 export default websiteRouter
