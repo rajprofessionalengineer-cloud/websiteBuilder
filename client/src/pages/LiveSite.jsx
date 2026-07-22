82f039cff2415b252a2dd41b54426a0c2ee08704
Raj Ratn Gupta <raj.professional.id.engineer@gmail.com>
2026-07-22 20:03:24 +0530

    Update LiveSite.jsx

--- client/src/pages/LiveSite.jsx
+++ client/src/pages/LiveSite.jsx
@@ -11,7 +11,7 @@ function LiveSite() {
     useEffect(() => {
         const handleGetWebsite = async () => {
             try {
-                const result = await axios.get(`${serverUrl}/api/website/get-by-slug/${id}`, { withCredentials: true })
+                const result = await axios.get(`${serverUrl}/api/website/get-by-slug/${id}`)
                 setHtml(result.data.latestCode)
             } catch (error) {
                 console.log(error)
